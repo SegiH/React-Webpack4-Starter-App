a67f7cd064f7a981f5e5cf2ea853aaacf416f363
Segi <segihovav@gmail.com>
2018-09-20 00:06:59 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import './app.css';
+
+class App extends React.Component{
+    render(){
+        return(
+            <div className="app">
+             <h1>Hello React</h1>
+            </div>
+        )
+    }
+}
+export default App;
